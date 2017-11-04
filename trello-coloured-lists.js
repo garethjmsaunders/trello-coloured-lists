e33951b809398c0bac08f8bc1c95bc890963d21c
Unknown <gareth@garethjmsaunders.co.uk>
2017-11-04 00:28:28 +0000

    FEAT (Trello colour columns) Rewrite
    Change colours to more coherent set.
    Reorganise rules by colour.
    Make code easier to read and update.
    Stack alternatives within the same rules, to save space.
    Add code to make text searched for case-insensitive.
    Add comments.

--- trello-coloured-lists.js
+++ trello-coloured-lists.js
@@ -1,47 +1,95 @@
 // ==UserScript==
-// @name Trello boards (generic)
-// @description Highlight columns
-// @include /^https?://trello\.com/?.*$/
+// @name         Trello colour columns
+// @namespace    http://www.garethjmsaunders.co.uk/trello-colour-columns/
+// @version      4.0.0
+// @description  Highlight columns in Trello
+// @author       Gareth J M Saunders
+// @include      https://trello.com/*
 // @require https://code.jquery.com/jquery-3.2.1.min.js
-// @version 3.0.0
-// @copyleft 2014–2017, Gareth J M Saunders
+// @grant        none
 // ==/UserScript==
-
-// TODO Create JSON array of columns: See http://www.w3schools.com/js/js_json.asp
-// TODO Add license
 
 $(document).ready(function() {
 
-    console.log('Hey! Trello boards Tampermonkey script is running...');
+    console.log('GJMS: Hey! Trello colour columns Tampermonkey script is running...');
+    console.log('GJMS: Loading colours...');
+    var red       = '#e06666', // indianred
+        orange    = '#f6b26b', // sandybrown
+        yellow    = '#ffd966', // khaki
+        green     = '#93c47d', // darkseagreen
+        blue      = '#6fa8dc', // cornflowerblue
+        violet    = '#674ea7', // slateblue
+        magenta   = '#a64d79', // royalheath
 
-    var black  = '#333333',
-        yellow = '#FFFA99',
-        purple = '#A98CC1',
-        green  = '#76C76D',
-        amber  = '#F9AD4A',
-        red    = '#B5355D',
-        white  = '#fff',
-        dark   = '#a9a9a9';
+        black     = '#333333', // nightrider
+        dark      = '#666666', // dimgray
+        white     = '#ffffff'; // white
+
+
+    /** Make jQuery :contains() selector case insensitive
+     *
+     * @version 1.0.0 2017-11-03
+     * @see https://css-tricks.com/snippets/jquery/make-jquery-contains-case-insensitive/
+     * @since 3.9.0
+     */
+
+    $.expr[":"].contains = $.expr.createPseudo(function(arg) {
+        return function( elem ) {
+            return $(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
+        };
+    });
+
+
+
+
+    /** Change colour of Trello columns
+     *  Triggered by hover on the page
+     *
+     * @version 4.0.0 2017-11-03
+     * @author Gareth J M Saunders <gareth@garethjmsaunders.co.uk>
+     * @license http://opensource.org/licenses/gpl-license.php, GNU Public License
+     * @since 3.9.0
+     */
 
     $('body').hover(function() {
-        $("h2:contains('@')").css('color', black).parents('.list').css('background', purple);
-        $("h2:contains('THIS')").css('color', black).parents('.list').css('background', yellow);
-        $("h2:contains('In progress')").css('color', black).parents('.list').css('background', yellow);
-        $("h2:contains('IN PROGRESS')").css('color', black).parents('.list').css('background', yellow);
-        $("h2:contains('Sprint')").css('color', black).parents('.list').css('background', green);
-        $("h2:contains('Inbox')").css('color', black).parents('.list').css('background', green);
-        $("h2:contains('GOALS')").css('color', black).parents('.list').css('background', green);
-        $("h2:contains('WEEKLY')").css('color', black).parents('.list').css('background', green);
-        $("h2:contains('MONTHLY')").css('color', black).parents('.list').css('background', green);
-        $("h2:contains('Backlog')").css('color', black).parents('.list').css('background', amber);
-        $("h2:contains('QA')").css('color', black).parents('.list').css('background', amber);
-        $("h2:contains('Bugs / issues')").css('color', black).parents('.list').css('background', red);
-        $("h2:contains('Meetings')").css('color', black).parents('.list').css('background', purple);
-        $("h2:contains('Consultancy')").css('color', black).parents('.list').css('background', purple);
-        $("h2:contains('Waiting for')").css('color', black).parents('.list').css('background', purple);
-        $("h2:contains('Project management')").css('color', black).parents('.list').css('background', purple);
-        $("h2:contains('DONE')").css('color', black).parents('.list').css('background', dark);
-        $("h2:contains('THE BOUNDARY OF CERTAINTY')").css('color', black).parents('.list').css('background', black);
-        $(".points").css('background', white);
+
+        console.log('GJMS: Changing column colours...');
+
+        // Red - danger
+        $("textarea:contains('Bugs'), textarea:contains('Issues'), textarea:contains('Fix')")
+        .parents('.list').css('background', red);
+
+        // Orange - warning
+        $("textarea:contains('On hold'), textarea:contains('Scheduled')")
+        .parents('.list').css('background', orange);
+
+        // Yellow - nearly ready
+        $("textarea:contains('QA'), textarea:contains('Testing')")
+        .parents('.list').css('background', yellow);
+
+        // Green - in progress
+        $("textarea:contains('In progress'), textarea:contains('Doing')")
+        .parents('.list').css('background', green);
+
+        // Blue - planning
+        $("textarea:contains('Ideas'), textarea:contains('Inbox'), textarea:contains('Planning')")
+        .parents('.list').css('background', blue);
+
+        // Violet, white text
+        $("textarea:contains('Waiting for')")
+        .css('color', white)
+        .parents('.list').css('background', violet);
+
+        // Magenta, white text
+        $("textarea:contains('Archive')")
+        .css('color', white)
+        .parents('.list').css('background', magenta);
+
+        // Dark Grey, white text - complete
+        $("textarea:contains('Reference'), textarea:contains('Done')")
+        .css('color', white)
+        .parents('.list').css('background', dark);
+
     });
+
 });
